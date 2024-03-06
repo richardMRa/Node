d493f8e529344f88ae1d8fa0445e63fecfef051b
richardMRa <richete05@gmail.com>
2024-03-06 18:58:59 -0500

    express middlewares

--- Starting with Node/2-/3.express.js
+++ Starting with Node/2-/3.express.js
@@ -6,19 +6,35 @@ const PORT = process.env.PORT ?? 3000
 app.disable('x-powered-by') //Disables x-powered-by header 
 
 
-//Middleware
-//All requests pass here before 
+//"Middleware" this is exectued for every request before reaching its corresponding function
+app.use(express.json()) //express native middleware does the same as below
+/*
 app.use((req, res, next) => {
-    console.log('primer middleware')
+    if (req.method !== 'POST') return next()
+    if (req.header('content-type') !== 'application/json') return next()
 
-    next()
+    let body = ''
+
+    req.on('data', chunk => {
+        body += chunk.toString()
+    })
+
+    req.on('end', () => {
+        const data = JSON.parse(body)
+        req.body = data
+        next()
+    })
+
+
 })
+*/
 
 app.get('/', (req, res) => {
     res.send('<h1> Home con express </h1>')
 })
 
 app.post('/pokesample', (req, res) => {
+    /*
     let body = ''
     //Listen Data event
     req.on('data', chunk => {
@@ -29,6 +45,9 @@ app.post('/pokesample', (req, res) => {
         const data = JSON.parse(body)
         res.status(201).json(data)
     })
+    this code was moved to "use"
+    */
+    res.status(201).json(req.body)
 })
 
 app.use((req, res) => {
